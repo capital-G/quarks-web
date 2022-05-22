d756f385f7177f6a85d8ed269b02a63cdeb01314
Dennis Scheiba <dennis.scheiba@gmx.de>
2022-05-22 15:44:54 +0200

    fix url for deployment

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/quarks-web/'
+  : '/',
+
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
