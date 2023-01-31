02448a63f0ecefab2f799e04ed82c85ab468aff4
Dennis Scheiba <dennis.scheiba@gmx.de>
2023-01-31 20:42:34 +0100

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <v-app>
     <v-main>
-      <QuarkSearch/>
+      <QuarkSearch />
+      <v-footer padless color="grey">
+        <v-col class="text-center" cols="6">
+          <strong><a href="https://github.com/capital-G/quarks-web/">github.com/capital-G/quarks-web/</a></strong>
+        </v-col>
+        <v-col class="text-center" cols="6">
+          <strong><a href="https://github.com/supercollider-quarks/quarks">github.com/supercollider-quarks/quarks</a></strong>
+        </v-col>
+      </v-footer>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import QuarkSearch from './components/QuarkSearch.vue'
+import QuarkSearch from "./components/QuarkSearch.vue";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     QuarkSearch,
@@ -19,5 +27,5 @@ export default {
   data: () => ({
     //
   }),
-}
+};
 </script>
